0a2f46b2b1baa5b881a83b8dbe277b8fe9152bea
Josep-Reyes <74124786+Josep-Reyes@users.noreply.github.com>
2024-07-12 02:36:23 -0500

    CREACION INTERFAZ

---
+++ main.js
@@ -0,0 +1,55 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+
+function buscaserenazgo(){
+
+let usuariop=document.getElementById("usuario");
+let passwordp=document.getElementById("password");
+let validador=false;
+
+let ruta='personal.json'
+fetch(ruta)
+.then(response=>response.json())
+.then(data=>mostrardatos(data))
+.then(error=>console.log(error))
+
+const mostrardatos=(data)=>{
+
+for(let i=0; i<data.length;i++){
+if(data[i].usuario===usuariop.value && data[i].password===passwordp.value){
+  if(data[i].turno==="MAÑANA"){
+    
+    window.location.href="principal_morning.html";
+  }else if(data[i].turno==="TARDE"){
+    window.location.href="principal_afternoon.html";
+  }else if(data[i].turno==="NOCHE"){
+    window.location.href="principal_night.html";
+  }else{
+    alert("NO TIENE TURNO EL SERENO ",data[i].name)
+  }
+}
+}
+if(validador===false){
+  
+  usuariop.value="";
+  passwordp.value="";
+  
+Swal.fire({
+  position: "top-end",
+  icon: "error",
+  title: "Datos Incorrectos",
+  showConfirmButton: false,
+  timer: 1000
+});
+
+} 
+
+}
+}
